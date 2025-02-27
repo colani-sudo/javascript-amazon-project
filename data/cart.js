2bf13ece9578478aaf58bddbd20a02d30fe203d1
Colani Matsenjwa <matsenjwacolani@gmail.com>
2025-02-27 13:29:32 +0200

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -5,17 +5,21 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart) {
   cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: 1
   },
   {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: 2
   }]; // Normalizing or deduplicating the data
 }
   
 
 export function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
+  // To wipe off localStorage from console: localStorage.removeItem('cart');
+  // localStorage.clear()
 }
 
 export function addToCart(productId) {
@@ -37,7 +41,8 @@ export function addToCart(productId) {
       // productId: productId,
       // quantity: selectedValue
       productId,    // shorthand property
-      quantity
+      quantity,
+      deliveryOptionId: '1'
     });
   }
   saveToStorage();
