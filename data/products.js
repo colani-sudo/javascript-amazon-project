e766f84cdb9e45e79023aea0a652349e2881fc57
Colani Matsenjwa <matsenjwacolani@gmail.com>
2025-03-21 22:55:44 +0200

    Convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,32 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStartsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return `${formatCurrency(this.priceCents)}`;
+  }
+}
+
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -657,7 +686,10 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+}); // Converting a regular products array objects into objects of Product class array
+
 
 export function getProduct(productId) {
   let matchingProduct;
